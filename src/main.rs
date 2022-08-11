4fce17fa9892ba2e7f585bd0e121525ce2ca62cd
andy salerno <andysalerno@gmail.com>
2022-08-10 19:06:13 -0700

    attach listener to public address

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ mod providers;
 async fn main() {
     let page = warp::path!().and_then(front_page_render);
 
-    warp::serve(page).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(page).run(([0, 0, 0, 0], 3030)).await;
 }
 
 async fn front_page_render() -> Result<impl warp::Reply, Infallible> {
