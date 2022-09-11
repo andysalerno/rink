302016d9c95a0ee2d2ca6ea637ee2f753ba4e6dc
Andy Salerno <andysalerno@gmail.com>
2022-09-11 15:20:24 -0700

    rotate

--- src/js.js
+++ src/js.js
@@ -1,7 +1,4 @@
 var rotate = function rotate() {
-    // var doc = document.getElementById("parent");
-    // doc.style.writingMode = "vertical-rl";
-    // var button = document.getElementById("button");
-    // button.textContent = window.navigator.userAgent;
-    document.documentElement.requestFullscreen();
+    var doc = document.getElementById("parent");
+    doc.style.writingMode = "vertical-rl";
 };
